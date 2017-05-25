99b7fb9232f984f1f7c3c3953d42dfe874c58cc8
mfmfneko <admin@mfnk.jp>
2017-05-26 01:47:33 +0900

    Re-Format code
    Signed-off-by: mfmfneko <admin@mfnk.jp>

--- src/takesi/japanese.php
+++ src/takesi/japanese.php
@@ -8,29 +8,29 @@ use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Utils;
 
-class japanese extends PluginBase implements Listener{
+class japanese extends PluginBase implements Listener {
 
-    public function OnEnable() {
+	public function OnEnable() {
 		$this->getLogger()->notice("This plugin made by takesi.");
 		$this->getLogger()->notice("If you have a bug etc https://github.com/Takesikaityo/OnlyJapanese");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-	
-	 public function onLogin(PlayerPreLoginEvent $event){
+
+	public function onLogin(PlayerPreLoginEvent $event) {
 		$player = $event->getPlayer();
 		$ip = $player->getAddress();
 		$name = $player->getName();
-		$info = json_decode(Utils::getURL("http://freegeoip.net/json/".$ip), true);
+		$info = json_decode(Utils::getURL("http://freegeoip.net/json/" . $ip), true);
 		$innet = str_replace("192.168.", "999.999.", $ip);
-		if($ip == $innet){
-			if(!$info['country_code'] == "JP") {
-			$player->kick("You arenot Japanese.", false);
-			$this->getLogger()->notice($name."が".$info['country_name']."の".$info['city']."からログインしようとしました。");
-		}else{
-			$this->getLogger()->notice($name."が".$info['country_name']."の".$info['city']."からログインしました。");
-		}
-		}else{
-			$this->getLogger()->notice($name."が内部インターネットからログインしました。");
+		if ($ip == $innet) {
+			if (!$info['country_code'] == "JP") {
+				$player->kick("You arenot Japanese.", false);
+				$this->getLogger()->notice($name . "が" . $info['country_name'] . "の" . $info['city'] . "からログインしようとしました。");
+			} else {
+				$this->getLogger()->notice($name . "が" . $info['country_name'] . "の" . $info['city'] . "からログインしました。");
+			}
+		} else {
+			$this->getLogger()->notice($name . "が内部インターネットからログインしました。");
 		}
 	}
 
